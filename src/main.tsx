e379c1d39a82afcd2147be88107f13e041d6d539
Juathan Coelho Duarte <juathanduarte13@gmail.com>
2025-09-08 16:23:12 -0300

    fix: ajustando blink no ios

--- src/main.tsx
+++ src/main.tsx
@@ -13,32 +13,6 @@ if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   setTheme("light");
 }
 
-const setViewportHeightVariable = () => {
-  const viewportHeightUnit = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${viewportHeightUnit}px`);
-};
-
-setViewportHeightVariable();
-
-let resizeRafId = 0 as number | undefined;
-const handleResizeViewport = () => {
-  if (resizeRafId) cancelAnimationFrame(resizeRafId);
-  resizeRafId = requestAnimationFrame(() => {
-    setViewportHeightVariable();
-  });
-};
-
-window.addEventListener("resize", handleResizeViewport, { passive: true });
-window.addEventListener("orientationchange", handleResizeViewport, {
-  passive: true,
-});
-
-if (window.visualViewport) {
-  window.visualViewport.addEventListener("resize", handleResizeViewport, {
-    passive: true,
-  });
-}
-
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found");
