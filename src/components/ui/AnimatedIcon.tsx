cabeb0ca8ef9ff72261c769859928a24d767881d
Juathan Coelho Duarte <juathanduarte13@gmail.com>
2025-09-08 15:40:09 -0300

    fix: ajustando animated-icon pois estava flickando no ios

--- src/components/ui/AnimatedIcon.tsx
+++ src/components/ui/AnimatedIcon.tsx
@@ -4,26 +4,36 @@ import mateIcon from "@assets/svgs/mate.svg";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
+const ICON_SOURCES = [mateIcon, hotBeverageIcon, laptopIcon];
+
 const AnimatedIcon = () => {
   const [currentIcon, setCurrentIcon] = useState<number>(0);
-  const icons = [mateIcon, hotBeverageIcon, laptopIcon];
+
+  useEffect(() => {
+    ICON_SOURCES.forEach((src) => {
+      const img = new Image();
+      img.src = src as unknown as string;
+    });
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIcon((prev) => (prev + 1) % icons.length);
+      setCurrentIcon((prev) => (prev + 1) % ICON_SOURCES.length);
     }, 2000);
 
     return () => clearInterval(interval);
-  }, [icons.length]);
+  }, []);
 
   return (
-    <div className="relative w-6 h-6 lg:w-8 lg:h-8">
-      <AnimatePresence mode="wait">
+    <div className="relative w-6 h-6 lg:w-8 lg:h-8 will-change-transform">
+      <AnimatePresence mode="wait" initial={false}>
         <motion.img
           key={currentIcon}
-          src={icons[currentIcon]}
+          src={ICON_SOURCES[currentIcon]}
           alt={`Icon ${currentIcon + 1}`}
-          className="w-full h-full hover:opacity-100 transition-opacity duration-200"
+          className="w-full h-full hover:opacity-100 transition-opacity duration-200 will-change-transform"
+          loading="eager"
+          decoding="async"
           initial={{
             opacity: 0,
             x: -10,
@@ -42,6 +52,10 @@ const AnimatedIcon = () => {
           transition={{
             duration: 0.5,
             ease: [0.68, -0.55, 0.27, 1.55],
+          }}
+          style={{
+            WebkitBackfaceVisibility: "hidden",
+            transform: "translateZ(0)",
           }}
         />
       </AnimatePresence>
